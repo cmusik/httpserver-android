654e1bf7cc6a0403836c645edb3d86225cf33e87
Christof Musik <christof@senfdax.de>
2019-01-01 14:44:23 +0100

    add missing permission to start on boot, also fix boot behavior on older android versions (fixes #5)

--- app/src/main/java/de/c11k/httpserver/AutoStart.kt
+++ app/src/main/java/de/c11k/httpserver/AutoStart.kt
@@ -3,13 +3,22 @@ package de.c11k.httpserver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.preference.PreferenceManager
 
 class AutoStart : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-        if (prefs.getBoolean("start_on_boot", true)) {
-            context!!.startForegroundService(Intent(context, BackgroundService::class.java))
+        if (intent!!.action == Intent.ACTION_BOOT_COMPLETED) {
+            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+            val serviceIntent = Intent(context, BackgroundService::class.java)
+            if (prefs.getBoolean("start_on_boot", true)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    context!!.startForegroundService(serviceIntent)
+                }
+                else {
+                    context!!.startService(serviceIntent)
+                }
+            }
         }
     }
 }
